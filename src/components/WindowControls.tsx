4ef122fc97192774638da132e50c9d7c93b00d71
bdorminey <briandorminey@sbp.com>
2025-08-18 12:46:17 -0400

    Add dynamic maximized state handling to WindowControls component

--- src/components/WindowControls.tsx
+++ src/components/WindowControls.tsx
@@ -1,20 +1,39 @@
-import type { JSX } from "solid-js";
-import { getCurrentWindow } from "@tauri-apps/api/window";
-import {VsChromeMinimize, VsChromeMaximize, VsChromeClose} from 'solid-icons/vs';
+import {createSignal, onCleanup, onMount, type JSX} from "solid-js";
+import {getCurrentWindow} from "@tauri-apps/api/window";
+import {VsChromeMinimize, VsChromeMaximize, VsChromeClose, VsChromeRestore} from 'solid-icons/vs';
 
 const appWindow = getCurrentWindow();
 
 export default function WindowControls(): JSX.Element {
-  const minimize = () => appWindow.minimize();
-  const toggleMax = async () =>
-    (await appWindow.isMaximized()) ? appWindow.unmaximize() : appWindow.maximize();
-  const close = () => appWindow.close();
+    const [maximized, setMaximized] = createSignal(false);
 
-  return (
-    <>
-        <button class="tb-btn" onClick={minimize} title="Minimize"><VsChromeMinimize size={24} color="red" /></button>
-        <button class="tb-btn" onClick={toggleMax} title="Zoom"><VsChromeMaximize size={24} color="red" /></button>
-        <button class="tb-btn danger" onClick={close} title="Close"><VsChromeClose size={24} color="red" /></button>
-    </>
-  );
+    const minimize = () => appWindow.minimize();
+    const toggleMax = async () =>
+        (await appWindow.isMaximized()) ? appWindow.unmaximize() : appWindow.maximize();
+    const close = () => appWindow.close();
+
+    const updateMaximized = async () => {
+        setMaximized(await appWindow.isMaximized());
+    }
+
+    onMount(async () => {
+        await updateMaximized();
+        const unlisten = await appWindow.onResized(async () => {
+            await updateMaximized();
+        });
+        onCleanup(() => {
+            unlisten();
+        });
+    });
+
+
+    return (
+        <>
+            <button class="tb-btn" onClick={minimize} title="Minimize"><VsChromeMinimize size={24} color="red"/></button>
+            <button class="tb-btn" onClick={toggleMax} title="Zoom">
+                {maximized() ? <VsChromeRestore size={24} color="red"/> : <VsChromeMaximize size={24} color="red"/>}
+            </button>
+            <button class="tb-btn danger" onClick={close} title="Close"><VsChromeClose size={24} color="red"/></button>
+        </>
+    );
 }
