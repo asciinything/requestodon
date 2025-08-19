91b09e08c1de633a4a72233d081eea8a4ff93dad
bdorminey <briandorminey@sbp.com>
2025-08-19 11:04:01 -0400

    Add maximize and unmaximize capabilities to default configuration

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import type { JSX } from "solid-js";
 export default function Search(): JSX.Element {
   return (
     <div class="search-bar">
-      <input type="text" placeholder="Search Postman" />
+      <input type="text" placeholder="Search Requestodon" />
     </div>
   );
 }
